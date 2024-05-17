ce8f477a4bbef4ee6afdcc3bee68f8b924049c1e
Borislav2-0 <crva20@mail.ru>
2024-05-17 13:22:43 +0300

    Add files via upload

---
+++ HM5.py
@@ -0,0 +1,13 @@
+my_list = ["UAZ", "VAZ", "BelAZ", "Chevrolet", "Ford", "Dodge"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:])
+my_list[2] = "Caterpillar"
+print(my_list)
+
+my_dict = {"cat": "кошка", "dog": "собака", "snake": "змея"}
+print(my_dict)
+print(my_dict["snake"])
+my_dict["crocodile"] = "крокодил"
+print(my_dict)
